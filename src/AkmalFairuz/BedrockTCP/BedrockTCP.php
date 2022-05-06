0dbbba1218c79eb5b27ae25abd0c68497055691f
AkmalFairuz <akmalf475@gmail.com>
2022-05-06 17:47:01 +0700

    Disable RakLibInterface & Query Interface

--- src/AkmalFairuz/BedrockTCP/BedrockTCP.php
+++ src/AkmalFairuz/BedrockTCP/BedrockTCP.php
@@ -6,20 +6,34 @@ namespace AkmalFairuz\BedrockTCP;
 
 use AkmalFairuz\BedrockTCP\network\TCPServerManager;
 use AkmalFairuz\BedrockTCP\network\TCPSession;
-use pocketmine\network\mcpe\raklib\RakLibInterface;
+use pocketmine\event\Listener;
+use pocketmine\event\server\NetworkInterfaceRegisterEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
+use function function_exists;
 
-class BedrockTCP extends PluginBase{
+class BedrockTCP extends PluginBase implements Listener{
 
     public function onEnable(): void{
+        if(!function_exists("zstd_compress")) {
+            $this->getLogger()->error("This plugin require zstd extension. Disabling plugin...");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            return;
+        }
         $ip = Server::getInstance()->getIp();
         $port = Server::getInstance()->getPort();
-        foreach($this->getServer()->getNetwork()->getInterfaces() as $interface) {
-            if($interface instanceof RakLibInterface) {
-                $interface->shutdown();
-            }
-        }
         $this->getServer()->getNetwork()->registerInterface(new TCPServerManager($this->getServer(), $this->getServer()->getNetwork(), $ip, $port, TCPSession::class));
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    /**
+     * @param NetworkInterfaceRegisterEvent $event
+     * @priority HIGHEST
+     */
+    public function onNetworkInterfaceRegister(NetworkInterfaceRegisterEvent $event) {
+        $interface = $event->getInterface();
+        if(!$interface instanceof TCPServerManager) {
+            $event->cancel();
+        }
     }
 }
