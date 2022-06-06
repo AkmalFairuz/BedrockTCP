98ffae57ff194c8edac6990cafe837f9f6055ff5
AkmalFairuz <akmalf475@gmail.com>
2022-06-06 19:48:12 +0700

    Packet flushing

--- src/AkmalFairuz/BedrockTCP/network/TCPPacketSender.php
+++ src/AkmalFairuz/BedrockTCP/network/TCPPacketSender.php
@@ -19,6 +19,9 @@ class TCPPacketSender implements PacketSender{
 
     public function send(string $payload, bool $immediate): void{
         $this->session->write(Binary::writeInt(strlen($payload)) . $payload);
+        if($immediate) {
+            $this->session->flush();
+        }
     }
 
     public function close(string $reason = "unknown reason"): void{
